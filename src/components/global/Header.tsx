ced7e328664bef2be2f0b9ef25be3f2db0ecb78b
efilho-topus <efilho@topus.group>
2025-05-04 16:55:57 -0300

    corrected image import

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router'
 export const Header = () => {
   return (
     <header className='w-full bg-dark-blue shadow-xl'>
-        <div className="mx-[1.25%] p-1 py-2">
+        <div className="mx-[2.25%] p-1 py-6">
             <NavLink to={'/'}>
-                <img src={diagnoxLogo}/>
+                <img className='h-18' src={diagnoxLogo}/>
             </NavLink>
         </div>
     </header>
