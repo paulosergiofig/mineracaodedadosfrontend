c2a56510a3c385da1430974c6653c9e26627ff56
efilho-topus <efilho@topus.group>
2025-05-05 10:34:23 -0300

    added dialog

--- src/features/home.tsx
+++ src/features/home.tsx
@@ -1,8 +1,19 @@
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+
 export const Home = () => {
   return (
-    <div className=' bg-gray-200'>
-      placeholderHome
-    </div>
+  <Dialog>
+  <DialogTrigger>Open</DialogTrigger>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
   )
 }
 
