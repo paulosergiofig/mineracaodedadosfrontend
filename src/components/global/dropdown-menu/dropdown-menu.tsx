4b44fc28c8634db6fde48a5cd11ae4303db57522
efilho-topus <efilho@topus.group>
2025-05-05 13:58:46 -0300

    pending styling

---
+++ src/components/global/dropdown-menu/dropdown-menu.tsx
@@ -0,0 +1,40 @@
+import { FC, useState } from 'react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+
+interface DropdownProps {
+    placeholderText: string
+    searchBarIconUrl?: string
+    options: {label: string, value: string}[]
+    opcaoSelecionada: any
+    setOpcaoSelecionada: (option: {label: string, value: string}) => void
+}
+
+export const Dropdown: FC<DropdownProps> = (props) => {
+  const { placeholderText, options, opcaoSelecionada, setOpcaoSelecionada, searchBarIconUrl } = props
+  const [open, setOpen] = useState<boolean>(false)
+  return (
+    <DropdownMenu open={!!open}>
+    <DropdownMenuTrigger onClick={() => setOpen(true)}>
+      {!!searchBarIconUrl && searchBarIconUrl}
+      {!!opcaoSelecionada ? opcaoSelecionada.label : placeholderText}
+    </DropdownMenuTrigger>
+    <DropdownMenuContent
+    onInteractOutside={() => setOpen(false)}
+    className='w-max max-w-full'>
+      {options.map((opcao) => {
+        return (
+            <DropdownMenuItem
+            key={`option-${opcao.value}`}
+            onSelect={() => {
+              setOpcaoSelecionada(opcao)
+              setOpen(false)
+            }}
+            >{opcao.label}</DropdownMenuItem>
+        )
+      })}
+    </DropdownMenuContent>
+  </DropdownMenu>
+  )
+}
+
+
--- src/features/home.tsx
+++ src/features/home.tsx
@@ -1,6 +1,10 @@
+import { Dropdown } from "@/components"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { useState } from "react"
 
 export const Home = () => {
+  const [opcaoSelecionada, setOpcaoSelecionada] = useState<{label: string, value: string}>()
+  console.log(opcaoSelecionada)
   return (
   <Dialog>
   <DialogTrigger>Open</DialogTrigger>
@@ -12,6 +16,12 @@ export const Home = () => {
         and remove your data from our servers.
       </DialogDescription>
     </DialogHeader>
+    <Dropdown 
+    placeholderText="Selecione XYZ" 
+    options={[{label: 'Opcao A', value: 'A'}, {label: 'Opcao B', value: 'B'}]}
+    opcaoSelecionada={opcaoSelecionada}
+    setOpcaoSelecionada={setOpcaoSelecionada}
+    />
   </DialogContent>
 </Dialog>
   )
