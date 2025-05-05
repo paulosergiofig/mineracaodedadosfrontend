2dfc6de4e7aef325812c6f6cccfedeca9a3c075a
efilho-topus <efilho@topus.group>
2025-05-05 17:21:22 -0300

    removed unrelated changes

--- src/features/home.tsx
+++ src/features/home.tsx
@@ -1,29 +1,16 @@
 import { Dropdown } from "@/components"
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { useState } from "react"
 
 export const Home = () => {
   const [opcaoSelecionada, setOpcaoSelecionada] = useState<{label: string, value: string}>()
   console.log(opcaoSelecionada)
   return (
-  <Dialog>
-  <DialogTrigger>Open</DialogTrigger>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
     <Dropdown 
     placeholderText="Selecione XYZ" 
     options={[{label: 'Opcao A', value: 'A'}, {label: 'Opcao B', value: 'B'}]}
     opcaoSelecionada={opcaoSelecionada}
     setOpcaoSelecionada={setOpcaoSelecionada}
     />
-  </DialogContent>
-</Dialog>
   )
 }
 
