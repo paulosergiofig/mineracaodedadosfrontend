0730a32bb49bf4e2cc97aa588a6aaaacb98956bd
efilho-topus <efilho@topus.group>
2025-05-05 00:33:56 -0300

    fixed routing

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -7,8 +7,8 @@ const Router = () => {
             <Routes>
                 <Route element={<AppLayout/>}>
                     <Route index element={<Home/>}/>
-                    <Route path="enviar-imagem" element={<EnvioDeImagem/>}/>
-                    <Route path="diagnostico/:parteDoCorpo/:doenca/:sexo" element={<Diagnostico/>}/>
+                    <Route path="enviar-imagem/:opcaoCorpo/:doenca/:sexo" element={<EnvioDeImagem/>}/>
+                    <Route path="diagnostico/:opcaoCorpo/:doenca/:sexo" element={<Diagnostico/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
