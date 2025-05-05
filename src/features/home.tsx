ca869af2b48da265416830e13635d1754aeffd5e
efilho-topus <efilho@topus.group>
2025-05-05 10:15:23 -0300

    Revert "base concluida"
    This reverts commit cbc4990f897063f1fac70914b4ba02122f2b6825.

--- src/features/home.tsx
+++ src/features/home.tsx
@@ -1,12 +1,7 @@
-import ContentBox from "@/components/diagnosis/ContentBox"
-
 export const Home = () => {
   return (
     <div className=' bg-gray-200'>
-      <ContentBox.Root>
-        <ContentBox.Title>Titulo</ContentBox.Title>
-        <ContentBox.Body>Corpo</ContentBox.Body>
-      </ContentBox.Root>
+      placeholderHome
     </div>
   )
 }
