d3bca35be2e5c0d61fff45fcabb88fb16ef88802
efilho-topus <efilho@topus.group>
2025-05-06 17:25:34 -0300

    done basic component

--- src/components/diagnostico/ContentBox.tsx
+++ src/components/diagnostico/ContentBox.tsx
@@ -1,23 +1,38 @@
 import { FC, PropsWithChildren } from "react"
 
-interface ContentBoxProps {
-    title?: string
+interface ContentBox {
+    Title: FC<PropsWithChildren<{className?: string}>>
+    Root: FC<PropsWithChildren<{className?: string}>>
+    Content: FC<PropsWithChildren<{className?: string}>>
 }
 
-export const ContentBox: FC<PropsWithChildren<ContentBoxProps>> = (props) => {
-    const { title, children } = props
+const Root: ContentBox['Title'] = ({children, className}) => {
     return (
-        <div className="
-         w-[400px] xl:w-[500px] 2xl:w-[700px] max-w-full h-max
-         rounded-lg shadow-2xl ">
-            {!!title && 
-            (<div className="bg-content-box-title mx-[8%] text-2xl py-7 font-medium">
-                {title}
-            </div>)}
-            <div className="bg-white rounded-lg p-6 px-10
-            h-[200px] xl:h-[380px] 2xl:h-[450px]">
-                {children}
-            </div>
-        </div>
-    )
+    <div className={`
+        w-[400px] xl:w-[500px] 2xl:w-[700px] max-w-full h-max
+         rounded-lg shadow-2xl ${className ?? ''}`}>
+            {children}
+    </div>)
 }
+const Title: ContentBox['Root'] = ({children, className}) => {
+    return (
+    <div className={` mx-[8%] text-lg xl:text-2xl py-7 font-medium ${className ?? ''}`}>
+        {children}
+    </div>)
+}
+
+const Content: ContentBox['Content'] = ({children, className}) => {
+    return ( 
+    <div className={`bg-white rounded-lg p-6 px-10
+    h-[200px] xl:h-[380px] 2xl:h-[450px] ${className ?? ''}`}>
+        {children}
+    </div>)
+}
+
+const ContentBox: ContentBox = () => {}
+
+ContentBox.Root = Root
+ContentBox.Title = Title
+ContentBox.Content = Content
+
+export {ContentBox}
