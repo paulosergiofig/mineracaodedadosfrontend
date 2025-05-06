c855b9e5e95f6d8fef520509dfe483b2533cc2e4
André Gustavo Xavier dos Santos <andregustavoxs@MacBook-Air-de-Andre.local>
2025-05-06 19:15:09 -0300

    feat: 404 error component

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink } from 'react-router';
+import NotFoundImage from '@/assets/imgs/404-asset.png';
+import { Header } from '../global/Header';
+
+const NotFound: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-[var(--color-white-background)]">
+      <Header />
+      <div className="flex flex-col items-center justify-center p-4" style={{ minHeight: 'calc(100vh - 96px)' }}>
+        <div className="w-full">
+          <div className="flex justify-center mb-8">
+            <img 
+              src={NotFoundImage} 
+              alt="Página não encontrada" 
+              className="w-[600px] max-w-full"
+            />
+          </div>
+          <h1 className="text-5xl font-bold text-center mb-4 text-[var(--color-dark-blue)]">OOPS!</h1>
+          <p className="text-center text-[var(--color-dark-blue)] text-lg mb-6">
+            Não conseguimos encontrar a página que você estava procurando.
+            <br />
+            Verifique o endereço digitado ou{' '}
+            <NavLink to="/" className="font-bold text-[var(--color-grayish-blue)] underline hover:text-[var(--color-blue)]">
+              volte ao início
+            </NavLink>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,5 @@
 import { AppLayout, Diagnostico, EnvioDeImagem, Home } from '@/features'
+import { NotFound } from '@/components'
 import { BrowserRouter, Route, Routes } from "react-router"
 
 const Router = () => {
@@ -10,6 +11,7 @@ const Router = () => {
                     <Route path="enviar-imagem/:opcaoCorpo/:doenca/:sexo" element={<EnvioDeImagem/>}/>
                     <Route path="diagnostico/:opcaoCorpo/:doenca/:sexo" element={<Diagnostico/>}/>
                 </Route>
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
     )
