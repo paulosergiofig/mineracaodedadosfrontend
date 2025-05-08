d853b693bee03cac7a9e2d220bb32c7ecab1fdc9
efilho-topus <efilho@topus.group>
2025-05-08 14:44:45 -0300

    pending styling && adapt to endpoint

--- src/components/diagnostico/file-picker.tsx
+++ src/components/diagnostico/file-picker.tsx
@@ -1,87 +1,111 @@
 import { FC } from "react";
 import ReactImageUploading from "react-images-uploading";
 import uploadIcon from "../../assets/imgs/icon_upload.png";
-// import confirmedIcon from "../../assets/imgs/icon_upload.png";
+import confirmedIcon from "../../assets/imgs/icon_confirm.png";
+import {largeSpinner} from '../../assets/spinner'
 
 interface FilePickerProps {
-  imagem: any,
-  setImagem: (imagem: any) => void
+  imagem: any;
+  setImagem: (imagem: any) => void;
+  serverResponseStatus?: string;
 }
 
 export const FilePicker: FC<FilePickerProps> = (props) => {
-  const {imagem, setImagem} = props
+  const { imagem, setImagem, serverResponseStatus } = props;
   const onChange = (imageList: any, addUpdateIndex: any) => {
     console.log(imageList, addUpdateIndex);
     setImagem(imageList);
   };
+
   return (
-    <div>
-      {/* <div
+    <div className="2xl:w-[650px] 2xl:h-[475px] xl:h-[500px] xl:w-[400px] rounded-md">
+      {serverResponseStatus === "" ? (
+        <ReactImageUploading
+          value={imagem}
+          onChange={onChange}
+          dataURLKey="data_url"
+        >
+          {({
+            imageList,
+            onImageUpload,
+            isDragging,
+            dragProps,
+            // errors
+          }) => (
+            <div
+              className="
+                  upload__image-wrapper
+                  w-fit h-fit
+                  "
+            >
+              <button
+                className="
+                      bg-light-blue 
+                      2xl:w-[650px] 2xl:h-[475px] xl:h-[500px] xl:w-[400px] rounded-md
+                      flex justify-center items-center hover:cursor-pointer border-dashed border-gray-500 border-2
+                      "
+                style={isDragging ? { color: "red" } : undefined}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                {imagem.length < 1 ? (
+                  <div className="flex flex-col items-center gap-[18px] justify-center">
+                    <img src={uploadIcon} alt="upload_icon" className="w-12" />
+                    <div>
+                      <h2 className="text-lg font-bold text-blue">
+                        Importe seu arquivo
+                      </h2>
+                      <h3 className="text-gray-500 text-sm font-normal"></h3>
+                    </div>
+                  </div>
+                ) : (
+                  <div
+                    className="flex flex-col items-center justify-center gap-3
+                       xl:w-[600px] xl:h-[400px]
+                       "
+                  >
+                    <img
+                      src={imagem[0]["data_url"]}
+                      alt=""
+                      className="max-w-full max-h-full object-contain"
+                    />
+                    {imageList[0].file?.name}
+                  </div>
+                )}
+              </button>
+              &nbsp;
+            </div>
+          )}
+        </ReactImageUploading>
+      ) : serverResponseStatus === "pending" ? (
+        <div
         className="
-            bg-light-green 
-            xl:min-w-[700px] 2xl:min-w-[650px] xl:min-h-[500px] 2xl:min-h-[500px]
-            flex justify-center items-center hover:cursor-pointer border-dashed border-gray-500 border-2
-            "
-        style={isDragging ? { color: "red" } : undefined}
-        onClick={onImageUpload}
-        {...dragProps}
+        bg-light-blue  
+          2xl:w-[650px] 2xl:h-[475px] xl:h-[500px] xl:w-[400px] rounded-md
+          flex justify-center items-center border-dashed border-gray-500 border-2
+          "
+      >
+        <div className="flex flex-col items-center justify-center gap-[18px]">
+          <div style={{
+            backgroundImage: largeSpinner
+          }} className="w-20 h-20 bg-no-repeat bg-center bg-contain pb-10"> </div>
+          <h2 className="text-md font-bold text-blue">Enviando arquivo...</h2>
+        </div>
+      </div>
+      ) : (
+        <div
+        className="
+        bg-light-green 
+          2xl:w-[650px] 2xl:h-[475px] xl:h-[500px] xl:w-[400px] rounded-md
+          flex justify-center items-center border-dashed border-gray-500 border-2
+          "
       >
         <div className="flex flex-col items-center gap-[22px] justify-center">
           <img src={confirmedIcon} alt="upload_icon" className="w-[35%]" />
-          <h2 className="text-md font-bold text-green">Arquivo enviado</h2>
+          <h2 className="text-lg font-bold text-green">Arquivo enviado</h2>
         </div>
-      </div> */}
-      <ReactImageUploading
-        value={imagem}
-        onChange={onChange}
-        dataURLKey="data_url"
-      >
-        {({
-          imageList,
-          onImageUpload,
-          isDragging,
-          dragProps,
-        }) => (
-          <div
-            className="
-          upload__image-wrapper
-          bg-grayish-blue w-fit h-fit
-          p-3"
-          >
-            <button
-              className="
-              bg-light-blue 
-              xl:min-w-[700px] 2xl:min-w-[650px] xl:min-h-[500px] 2xl:min-h-[500px]
-              flex justify-center items-center hover:cursor-pointer border-dashed border-gray-500 border-2
-              "
-              style={isDragging ? { color: "red" } : undefined}
-              onClick={onImageUpload}
-              {...dragProps}
-            >
-              {imagem.length < 1 ? 
-              <div className="flex flex-col items-center gap-[18px] justify-center">
-                <img src={uploadIcon} alt="upload_icon" className="w-[20%]" />
-                <div>
-                  <h2 className="text-lg font-bold text-blue">
-                    Importe seu arquivo
-                  </h2>
-                  <h3 className="text-gray-500 text-sm font-normal">
-                    Arraste ou clique pra fazer upload
-                  </h3>
-                </div>
-              </div> :
-              <div className="flex flex-col items-center justify-center gap-3
-               xl:w-[600px] xl:h-[400px]
-               ">
-               <img src={imagem[0]["data_url"]} alt="" className="max-w-full max-h-full object-contain" /> 
-                {imageList[0].file?.name}
-              </div>
-              }
-            </button>
-            &nbsp;
-          </div>
-        )}
-      </ReactImageUploading>
+      </div>
+      )}
     </div>
   );
 };
--- src/features/home.tsx
+++ src/features/home.tsx
@@ -3,10 +3,23 @@ import { useState } from "react";
 
 export const Home = () => {
   const [image, setImage] = useState([]);
-  
+  const [r, setR] = useState('')
+
   return (
     <div className="h-full">
-      <FilePicker imagem={image} setImagem={setImage}/>
+      <div className="p-3 flex justify-center">
+      <FilePicker imagem={image} setImagem={setImage} serverResponseStatus={r}/>
+
+      </div>
+      <button onClick={() => {
+          setR('uploaded')
+      }}>upload</button>
+            <button onClick={() => {
+          setR('pending')
+      }}>pend</button>
+                  <button onClick={() => {
+          setR('')
+      }}>null</button>
     </div>
   )
 }
