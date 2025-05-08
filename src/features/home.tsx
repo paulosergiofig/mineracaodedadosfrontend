635a006c98f8ce425f920ee5e4253044900b3ac3
efilho-topus <efilho@topus.group>
2025-05-08 14:49:51 -0300

    cleaned home

--- src/features/home.tsx
+++ src/features/home.tsx
@@ -1,25 +1,8 @@
-import { CaixaDeUpload } from "@/components";
-import { useState } from "react";
 
 export const Home = () => {
-  const [image, setImage] = useState([]);
-  const [r, setR] = useState('')
-
   return (
     <div className="h-full">
-      <div className="p-3 flex justify-center">
-      <CaixaDeUpload imagem={image} setImagem={setImage} serverResponseStatus={r}/>
-
-      </div>
-      <button onClick={() => {
-          setR('uploaded')
-      }}>upload</button>
-            <button onClick={() => {
-          setR('pending')
-      }}>pend</button>
-                  <button onClick={() => {
-          setR('')
-      }}>null</button>
+      Home
     </div>
   )
 }
