1c0b201f8d80f617a0a5b81e708921094a43d2ed
efilho-topus <efilho@topus.group>
2025-05-12 14:34:42 -0300

    twMerge implementation

--- src/components/diagnostico/content-box.tsx
+++ src/components/diagnostico/content-box.tsx
@@ -1,32 +1,32 @@
 import { FC, PropsWithChildren } from "react"
+import { twMerge } from "tailwind-merge"
 
 interface ContentBox {
     Title: FC<PropsWithChildren<{className?: string}>>
     Root: FC<PropsWithChildren<{className?: string}>>
     Content: FC<PropsWithChildren<{className?: string}>>
 }
-// 06-05: className nao funciona em sobrescrever estilos ja declarados.
 
 const Root: ContentBox['Root'] = ({children, className}) => {
     return (
-    <div className={`
+    <div className={twMerge(`
         w-[400px] xl:w-[500px] 2xl:w-[700px] max-w-full h-max
-         rounded-lg shadow-2xl overflow-y  ${className ?? ''}`}>
+         rounded-lg shadow-2xl overflow-y  ${className ?? ''}`)}>
             {children}
     </div>)
 }
 const Title: ContentBox['Title'] = ({children, className}) => {
     console.log(className)
     return (
-    <div className={` mx-[8%] text-lg xl:text-2xl py-4 xl:py-7 font-medium ${className ?? ''}`}>
+    <div className={twMerge(` mx-[8%] text-lg xl:text-2xl py-4 xl:py-7 font-medium ${className ?? ''}`)}>
         {children}
     </div>)
 }
 
 const Content: ContentBox['Content'] = ({children, className}) => {
     return ( 
-    <div className={`bg-white rounded-b-lg p-6 px-10
-    h-[200px] xl:h-[380px] 2xl:h-[450px] overflow-y-auto ${className ?? ''}`}>
+    <div className={twMerge(`bg-white rounded-b-lg p-6 px-10
+    h-[200px] xl:h-[380px] 2xl:h-[450px] overflow-y-auto ${className ?? ''}`)}>
         {children}
     </div>)
 }
